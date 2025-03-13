4fa61542a6a449378e5e8baf9939126316f06760
psanant1208 <psanant1208@gmail.com>
2025-03-13 19:25:49 +0100

    Primer paso ejercicio Espiral.
    Crear de variables, constructor y declarar métodos.

---
+++ Espiral/src/main/java/model/Espiral.java
@@ -0,0 +1,54 @@
+package main.java.model;
+
+public class Espiral {
+	
+	protected int[][] matriz;
+	protected int dimension;
+	protected int[] centro;
+	
+	public Espiral(int[][] matriz) {
+		super();
+		this.matriz = matriz;
+		this.dimension = matriz.length;
+		this.centro = calcularCentro();
+	}
+	
+	public int[] calcularCentro() {
+		
+	}
+	
+	public void sumarEstrellas(int x, int y) {
+		
+	}
+	
+	public void comenzarSumatorio() {
+		
+	}
+	
+	public void desplazarArriba(int numPaso) {
+		
+	}
+	
+	public void desplazarIzquierda(int numPaso) {
+		
+	}
+	
+	public void desplazarAbajo(int numPaso) {
+		
+	}
+	
+	public void desplazarDerecha(int numPaso) {
+		
+	}
+
+	
+}
+
+
+
+
+
+
+
+
+
