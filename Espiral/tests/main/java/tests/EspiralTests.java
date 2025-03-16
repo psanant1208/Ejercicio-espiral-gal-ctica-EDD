6f401a3b1d31aa35d1442ebcefc996a11f826d69
Jose Antonio Espino <joseantespinodz@gmail.com>
2025-03-16 12:53:46 +0100

    añado la libreria de junit y creo espiraltests

---
+++ Espiral/tests/main/java/tests/EspiralTests.java
@@ -0,0 +1,13 @@
+package main.java.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import main.java.model.Espiral;
+
+class EspiralTests {
+	
+	
+
+}
